f1b607b0f9116bf84d9cf0a928042f3743bdef08
Frostdir <relaks_maks@mail.ru>
2022-01-31 19:49:31 +0300

    Скрытие формы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Habit} from "../models";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'habit-tracker';
+  public adding = false;
+  public habits: Habit[] = [
+    <Habit>{
+      name: '15 Minute Walk',
+      frequency: 'Daily',
+      description:
+        'This habit makes my kitchen look nice and makes my day better the next morning.',
+    },
+    <Habit>{
+      name: 'Weed the Garden',
+      frequency: 'Weekly',
+      description:
+        'The weeds get so out of hand if they wait any longer, and I like how nice our home looks with a clean lawn.',
+    },
+  ];
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule} from "@angular/material/card";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -17,6 +22,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserAnimationsModule,
     MatIconModule,
     MatToolbarModule,
+    MatCardModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
